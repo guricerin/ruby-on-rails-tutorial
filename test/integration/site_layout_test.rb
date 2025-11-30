3250e1cf3cec8c9308cde27ee330dc9d951fc900
guricerin <chanbo1e9@gmail.com>
2025-11-30 18:06:34 +0900

    integration test

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test "layout links" do
+    # rootのHTMLを取得
+    get root_path
+    # Homeページをレンダリングしているか
+    assert_template 'static_pages/home'
+    # リンクの数とリンク先は正しいか
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+  end
+end
