d4742b1744743f72cf8d8c47d50c822946557fc8
guricerin <chanbo1e9@gmail.com>
2025-12-07 19:31:50 +0900

    unique email

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_30_132945) do
+ActiveRecord::Schema[7.2].define(version: 2025_12_07_102244) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
