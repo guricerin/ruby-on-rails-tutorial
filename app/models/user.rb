987374e2e5f51197cbe6ed9a61b03d2e645ace8d
guricerin <chanbo1e9@gmail.com>
2025-12-07 17:40:46 +0900

    limit user field

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  validates(:name, presence: true) # 空文字を不許可
-  validates(:email, presence: true)
+  # presence: true => 空文字を不許可
+  validates(:name, presence: true, length: { maximum: 50 })
+  validates(:email, presence: true, length: { maximum: 255 })
 end
